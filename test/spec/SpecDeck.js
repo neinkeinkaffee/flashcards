17d31b7eeb4bd34f769837e52dcb2148f5342c33
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-04-27 11:47:01 +0200

    Replace all double by single quotes

--- test/spec/SpecDeck.js
+++ test/spec/SpecDeck.js
@@ -2,8 +2,8 @@ describe('Testing manipulation of the flashcard deck', ()=> {
     it('should add a new flashcard', () => {
         let deck = new Deck()
         let card = {
-            chinese: "咖啡",
-            english: "coffee"
+            chinese: '咖啡',
+            english: 'coffee'
         }
         deck.add(card)
         expect(deck.getCards().length).toBe(1)
@@ -11,12 +11,12 @@ describe('Testing manipulation of the flashcard deck', ()=> {
     it('should delete a flashcard', () => {
         let deck = new Deck()
         let card1 = {
-            chinese: "咖啡",
-            english: "coffee"
+            chinese: '咖啡',
+            english: 'coffee'
         }
         let card2 = {
-            chinese: "茶",
-            english: "tea"
+            chinese: '茶',
+            english: 'tea'
         }
         id1 = deck.add(card1)
         id2 = deck.add(card2)
@@ -25,4 +25,5 @@ describe('Testing manipulation of the flashcard deck', ()=> {
         expect(deck.getCards().length).toBe(1);
         expect(deck.getCards()[0].uuid).toBe(id1)
     })
+    it()
 })
