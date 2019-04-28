6990d7faa0ef10e76e7100f0a80191526b225d7a
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-04-28 21:41:20 +0200

    Remove worker settings that don't modify defaults anyway

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
 FROM nginx:alpine
 COPY src /usr/share/nginx/html
+COPY entrypoint.sh /entrypoint.sh
+RUN apk --no-cache add openssl && \
+    openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/private/snakeoil-key.pem -out /etc/ssl/snakeoil-cert.pem -days 365 -nodes -subj '/CN=localhost' && \
+    apk del --purge openssl
+CMD ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu -o pipefail
+
+echo "hello world"
+
+KEY_FILE=${KEY_FILE:-/etc/ssl/private/snakeoil-key.pem}
+CERT_FILE=${CERT_FILE:-/etc/ssl/snakeoil-cert.pem}
+
+cat > /etc/nginx/nginx.conf <<EOF
+http {
+
+    server {
+        listen 443 ssl;
+
+        ssl_certificate     "$CERT_FILE";
+        ssl_certificate_key "$KEY_FILE";
+        ssl_protocols       TLSv1.2;
+        ssl_ciphers         HIGH:!aNULL:!MD5;
+
+        location / {
+            root   /usr/share/nginx/html;
+            index  index.html index.htm;
+        }
+    }
+}
+EOF
+
+nginx -g "daemon off;"
