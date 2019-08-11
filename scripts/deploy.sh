5adb71449d61de41880c3535b466b1634665564c
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-08-11 11:39:21 +0200

    Deploy by opening port and proxying command

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eox pipefail
+
+. ./lib.sh
+
+
+
---
+++ scripts/lib.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+alias pi_exec='ssh -t -o "StrictHostKeyChecking no" $PROXY_USER@$PROXY_HOST ssh $DEPLOY_USER@$DEPLOY_HOST'
+
+# Credits go to https://advancedweb.hu/2019/04/02/sg_allow_ip/
+function open_port() {
+    PORT=$1
+
+    echo "Find the ID of the EC2 instance's security group"
+    SG=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
+    --query "Reservations[].Instances[].SecurityGroups[].GroupId" \
+    --no-paginate | jq -r '.[0]')
+
+    # Find the public IP of this machine
+    while : ; do
+        MYIP=$(curl -s ifconfig.me)
+        [ -z "$MYIP" ] || break
+    done
+
+    # Allow access for public IP of this machine
+    [ -z $(echo "$CIDRS" | grep "$MYIP/32") ] && aws ec2 authorize-security-group-ingress \
+        --group-id $SG --protocol tcp --port $PORT --cidr "$MYIP/32"
+}
+
+function close_port() {
+    PORT=$1
+
+    # Find the ID of the EC2 instance's security group
+    SG=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
+    --query "Reservations[].Instances[].SecurityGroups[].GroupId" \
+    --no-paginate | jq -r '.[0]')
+
+    # Find currently allowed IP ranges (variable interpolation in jq isn't working as documented)
+    CIDRS=$(aws ec2 describe-security-groups --group-ids $SG \
+        | jq -r '.SecurityGroups[].IpPermissions[]
+        | select(.FromPort == env.PORT and .ToPort == env.PORT and .IpProtocol == "tcp") | .IpRanges[].CidrIp')
+
+    # Revoke access for currently allowed IP ranges
+    for ip in $CIDRS; do
+        [ "$MYIP/32" != "$ip" ] && aws ec2 revoke-security-group-ingress \
+            --group-id $SG --protocol tcp --port $PORT --cidr $ip
+    done
+}
+
+function kubectl_apply() {
+    TEMPLATE_FILE=$1
+
+    cat <<EOF | kubectl apply -f -
+$(cat $TEMPLATE_FILE)
+EOF
+}
