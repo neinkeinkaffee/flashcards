b457b09a3221192e0da1c01c01f62d15149b3e81
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-08-16 16:00:52 +0200

    Make deploy script executable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,9 @@ set -eox pipefail
 
 . ./lib.sh
 
-
+open_port $CI_PORT
+exec_on_pi kubectl get pods
+copy_to_pi ./kubernetes/secret.yml
+exec_on_pi kubectl apply -f secret.yml
+exec_on_pi rm secret.yml
 
