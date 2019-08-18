aef10f4331347ab7a5952cda6daf013489db9cbc
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-08-18 11:06:54 +0200

    Switch pvc storageClassName to manual

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -eox pipefail
 . ./scripts/lib.sh
 
 open_port $CI_PORT
-kubectl_apply persistent-volume-hostpath.yml persistent-volume-claim.yml
+kubectl_apply persistent-volume-hostpath.yml persistent-volume-claim-hostpath.yml
 kubectl_apply secret.yml
 kubectl_apply postgres-deployment.yml postgres-service.yml
 kubectl_apply flask-deployment.yml flask-service.yml
