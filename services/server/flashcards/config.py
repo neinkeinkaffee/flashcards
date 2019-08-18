3316a1190eb1340d7264d0c38b7a631d850a25f7
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-08-18 17:24:58 +0200

    Add integration test

--- services/server/flashcards/config.py
+++ services/server/flashcards/config.py
@@ -3,7 +3,7 @@ import os
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 DATABASE_URL = f'postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/flashcards'
-
+IN_MEMORY_DATABASE_URL = f'sqlite://'
 
 class BaseConfig:
     """Base configuration"""
@@ -11,6 +11,10 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class TestConfig(BaseConfig):
+    """Development configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = IN_MEMORY_DATABASE_URL
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
---
+++ services/server/flashcards/tests/test_db_integration.py
@@ -0,0 +1,41 @@
+import os, pytest
+
+from flashcards import create_app, db
+from flashcards.api.model import Flashcard
+
+os.environ['APP_SETTINGS'] = 'flashcards.config.TestConfig'
+
+def setup_function(function):
+    app = create_app()
+    ctx = app.app_context()
+    ctx.push()
+    db.create_all()
+
+def teardown_function(function):
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+def test_adding_new_card():
+    db.session.add(Flashcard(
+        chinese='吃了嗎？',
+        english='Have you eaten?'))
+    db.session.commit()
+
+    flashcards = Flashcard.query.all()
+    assert len(flashcards) == 1
+    assert Flashcard.query.all()[0].english == 'Have you eaten?'
+
+def test_adding_two_new_cards():
+    db.session.add(Flashcard(
+        chinese='夠了嗎？',
+        english='Enough?'))
+    db.session.add(Flashcard(
+        chinese='吃了嗎？',
+        english='Have you eaten?'))
+    db.session.commit()
+
+    flashcards = Flashcard.query.all()
+    assert len(flashcards) == 2
+    assert flashcards[0].english == 'Enough?'
+    assert flashcards[1].english == 'Have you eaten?'
