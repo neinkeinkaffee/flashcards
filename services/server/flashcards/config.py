f920da3df84a87dace69fe1f0b08dce576ff012e
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-08-22 10:19:18 +0200

    Remove TestConfig from config

--- services/server/flashcards/config.py
+++ services/server/flashcards/config.py
@@ -3,18 +3,12 @@ import os
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 DATABASE_URL = f'postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/flashcards'
-IN_MEMORY_DATABASE_URL = f'sqlite://'
 
 class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class TestConfig(BaseConfig):
-    """Development configuration"""
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = IN_MEMORY_DATABASE_URL
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
