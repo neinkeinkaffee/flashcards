b6fd29a86004149fb557444e4b1ea8f674de65fa
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-10-09 14:05:56 +0800

    Remove obsolete parameter

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ set -eox pipefail
 . ./scripts/lib.sh
 
 build_and_push ./services/server flashcards-flask
-build_and_push ./services/client flashcards-nginx BASE_URL
+build_and_push ./services/client flashcards-nginx
 
 
