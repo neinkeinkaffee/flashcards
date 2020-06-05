2fd539c61c11426cfb832d7f2928ab51c4264cb3
Gesa Stupperich <gesa.stupperich@gmail.com>
2020-06-05 20:43:27 +0200

    Delete hostpath and local pvc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,8 @@ set -eox pipefail
 . ./scripts/lib.sh
 
 open_port $CI_PORT
-kubectl_apply persistent-volume-hostpath.yml persistent-volume-claim-hostpath.yml
 kubectl_apply secret.yml
-kubectl_apply postgres-deployment.yml postgres-service.yml
+kubectl_apply postgres-deployment.yml postgres-service.yml postgres-pvc.yml
 kubectl_delete_pod flask || kubectl_apply flask-deployment.yml flask-service.yml
 kubectl_delete_pod nginx || kubectl_apply nginx-deployment.yml nginx-service.yml
 kubectl_exec postgres createdb -U postgres flashcards || true
