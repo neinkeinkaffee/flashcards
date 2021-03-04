b2bad6d654d200f57d11604305337a5a01f146e9
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-03-04 11:41:28 +0100

    Call e2e tests when e2e parameter gets passed

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-pushd test
+function e2e_test() {
+  pushd test
+  docker-compose down
+  docker-compose up -d --build
+  docker-compose exec flask python manage.py recreate_db
+  docker-compose exec flask python manage.py seed_db
+  docker-compose run e2e py.test
+  docker-compose down
+  popd
+}
 
-docker-compose down
-docker-compose up -d --build
-docker-compose exec flask python manage.py recreate_db
-docker-compose exec flask python manage.py seed_db
-
-docker-compose run e2e py.test
-docker-compose down
-
-popd
+CMD=${1:-}
+shift || true
+case ${CMD} in
+e2e) e2e_test ;;
+esac
