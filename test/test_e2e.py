fb326825d3e826721c31ef5a5459ef2c0f84217d
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-04-24 18:55:40 +0100

    Set BASE_URL to frontend container DNS name

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,25 +1,14 @@
 def test_browser_title_contains_app_name(homepage):
     assert 'Flashcards' in homepage.title
 
-def test_page_heading_is_flashcards(homepage):
+def test_heading_is_flashcards(homepage):
     heading = homepage.find_element_by_id('heading').text
     assert 'Flashcards' == heading
 
-# def test_page_has_input_for_text(self):
-#     input_element = self._find('input-element')
-#     self.assertIsNotNone(input_element)
-#
-# def test_page_has_button_for_submitting_text(self):
-#     submit_button = self._find('find-button')
-#     self.assertIsNotNone(submit_button)
-#
-# def test_page_has_ner_table_on_it(self):
-#     input_element = self._find('input-element')
-#     submit_button = self._find('find-button')
-#     input_element.send_keys('France and Germany share a border in Europe')
-#     submit_button.click()
-#     ner_table = self._find('ner-table')
-#     self.assertIsNotNone(ner_table)
+def test_displays_random_card(homepage):
+    phrase_displayed = homepage.find_element_by_id('card').text
+    print(phrase_displayed)
+    assert 0 < len(phrase_displayed)
 
 def _find(self, val):
     return self.driver.find_element_by_id(val)
