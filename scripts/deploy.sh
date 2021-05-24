360c2a355699644f06d537b7919b90600768cedd
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-05-24 10:13:26 +0100

    Install terraform in python ci container fully

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,11 @@
 set -eox pipefail
 . ./scripts/lib.sh
 
-COMMIT_HASH=$1
-
+COMMIT_HASH=latest
+if [[ $(has_diff_since_last_build ./services $LAST_BUILD_COMMIT_HASH) ]]
+then
+  COMMIT_HASH=$CIRCLE_SHA1
+fi
 terraform_apply $COMMIT_HASH
 kubectl_exec postgres createdb -U postgres flashcards || true
 kubectl_exec flask python3 manage.py recreate_db
