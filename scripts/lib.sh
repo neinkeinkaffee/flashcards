9e45b7394f127b42c74052641f99ac4e71b48133
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-05-24 11:44:15 +0100

    Overwrite KUBECONFIG value

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,72 +1,9 @@
 #!/usr/bin/env bash
 
-function exec_on_pi() {
-    ssh -t -o "StrictHostKeyChecking no" $PROXY_USER@$PROXY_HOST ssh $DEPLOY_USER@$DEPLOY_HOST "$@"
-}
-
-function copy_to_pi() {
-    local FILE=$1
-    scp -o ProxyCommand="ssh -W %h:%p $PROXY_USER@$PROXY_HOST" -o "StrictHostKeyChecking no" $FILE $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIR
-}
-
-# Credits go to https://advancedweb.hu/2019/04/02/sg_allow_ip/
-function open_port() {
-    local PORT=$1
-
-    echo "Find the ID of the EC2 instance's security group"
-    local SG=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
-    --query "Reservations[].Instances[].SecurityGroups[].GroupId" \
-    --no-paginate | jq -r '.[0]')
-
-    # Find the public IP of this machine
-    while : ; do
-        local MYIP=$(curl -s ifconfig.me)
-        [ -z "$MYIP" ] || break
-    done
-
-    echo "Allow access for public IP of this machine"
-    aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port $PORT --cidr "$MYIP/32"
-}
-
-function close_port() {
-    local PORT=$1
-
-    echo "Find the ID of the EC2 instance's security group"
-    local SG=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=proxy" \
-    --query "Reservations[].Instances[].SecurityGroups[].GroupId" \
-    --no-paginate | jq -r '.[0]')
-
-    echo "Find currently allowed IP ranges"
-    local CIDRS=$(aws ec2 describe-security-groups --group-ids $SG \
-        | jq -r --argjson PORT "$PORT" '.SecurityGroups[].IpPermissions[]
-        | select(.FromPort == $PORT and .ToPort == $PORT and .IpProtocol == "tcp") | .IpRanges[].CidrIp')
-
-    echo "Revoke access for currently allowed IP ranges"
-    for ip in $CIDRS; do
-        aws ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port $PORT --cidr $ip
-    done
-}
-
 function grep_pod_id() {
     local POD_NAME=$1
-    local POD_ID=$(kubectl get pods | grep -o $POD_NAME-[-0-9a-z]*)
+    local POD_ID=$(KUBECONFIG=kubeconfig kubectl get pods | grep -o $POD_NAME-[-0-9a-z]*)
     echo $POD_ID
-}
-
-function kubectl_apply() {
-    for FILE in "$@"
-    do
-        exec_on_pi sudo kubectl get pods
-        copy_to_pi ./kubernetes/$FILE
-        exec_on_pi sudo kubectl apply -f $FILE
-        exec_on_pi rm $FILE
-    done
-}
-
-function kubectl_delete_pod() {
-    local POD_NAME=$1
-    local POD_ID=$(grep_pod_id $POD_NAME)
-    exec_on_pi sudo kubectl delete pod $POD_ID
 }
 
 function copy_qemu_bin {
@@ -111,7 +48,7 @@ function kubectl_exec() {
     local POD_NAME=${PARAMS[0]}
     local COMMAND=${PARAMS[@]:1}
     local POD_ID=$(grep_pod_id $POD_NAME)
-    kubectl exec $POD_ID --stdin -- $COMMAND
+    KUBECONFIG=kubeconfig kubectl exec $POD_ID --stdin -- $COMMAND
 }
 
 function has_diff_since_last_build() {
